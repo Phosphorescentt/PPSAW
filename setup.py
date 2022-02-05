e2e20035649fe0e9cd16ae3609b33336c7047af2
Joshua Mankelow <joshua@mankelow.org>
2022-02-05 23:14:58 +0000

    First proper commit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name = "PPSAW",
     version = "0.0.1",
     author = "Joshua \"Phosphorescent\" Mankelow",
-    description = "A Python PandaScore API Wrapper"
+    description = "A Python PandaScore API Wrapper",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/Phosphorescentt/PPSAW",
@@ -16,7 +16,7 @@ setuptools.setup(
         },
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (AGPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
         ],
     package_dir = {"": "src"}, 
